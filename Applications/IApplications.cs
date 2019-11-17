7e4755b02011b028bd62befe775fba6c4c6e625f
ctrpetersen <ctrpetersen@outlook.com>
2019-11-17 19:33:28 +0100

    strange naming convention fixed, now interface

---
+++ Applications/IApplications.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Warden.Applications
+{
+    interface IApplication
+    {
+        string AppPath { get; }
+        List<string> ErrorList { get; }
+        int Timeout { get; }
+        bool ShouldPrint { get; }
+        Process Process { get; set; }
+
+        void StartApp();
+        void CloseApp();
+    }
+}
---
+++ Applications/Mantis.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Warden.Applications
+{
+    class Mantis : IApplication
+    {
+        public string AppPath => "C:\\Users\\zirr_\\source\\repos\\Mantis\\bin\\Release\\netcoreapp3.0\\Mantis.exe";
+
+        public List<string> ErrorList => null;
+
+        public int Timeout => 1000;
+
+        public bool ShouldPrint => true;
+
+        public Process Process { get; set; }
+
+
+        public void StartApp()
+        {
+            Process = new Process();
+        }
+
+        public void CloseApp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
